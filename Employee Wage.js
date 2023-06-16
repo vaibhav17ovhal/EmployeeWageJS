4379690b88dc9907f76eddbef5e86e8cc08352d7
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-16 22:58:14 +0530

    [Vaibhav] Add:Storing daily wage and total wage using array

--- Employee Wage.js
+++ Employee Wage.js
@@ -1,5 +1,5 @@
 //UC1
-
+/*
 {
     const IS_Absent = 1;
     var empCheck = Math.floor(Math.random() * 10) % 2;
@@ -12,7 +12,7 @@
         console.log("Employee is present");
     }
 }
-
+*/
 //UC2
 {
     const Part_Time = 1;
@@ -59,13 +59,13 @@
     {
         case Part_Time:
             return Part_Time_Hrs;
-            break;
+            
         case Full_Time:
             return Full_Time_Hrs;
-            break;
+            
         default:
             return 0;
-            break;
+            
     }
     }
     empHrs = getWorkingHours(empCheck);
@@ -105,7 +105,7 @@
         total_emp_Hrs += getWorkingHours(empCheck); 
     }
     let empWage = total_emp_Hrs * Wage_Per_Hr;
-    console.log("Hours: " + total_emp_Hrs + " Emp Wage: " + empWage);
+    console.log("Hours: " + total_emp_Hrs + ", Emp Wage: " + empWage);
 }
 
 //UC5
@@ -142,6 +142,57 @@
         total_emp_Hrs += getWorkingHours(empCheck); 
     }
     let empWage = total_emp_Hrs * Wage_Per_Hr;
-    console.log("Total Hours: " + total_emp_Hrs + " Emp Wage: " + empWage + " Total Days: " + total_working_Days);
+    console.log("Total Hours: " + total_emp_Hrs + ", Emp Wage: " + empWage + ", Total Days: " + total_working_Days);
 }
+
+//UC6
+
+{
+    const Part_Time = 1;
+    const Full_Time = 2;
+    const Part_Time_Hrs = 4;
+    const Full_Time_Hrs = 8;
+    const Wage_Per_Hr = 20;
+    const Max_Working_Days = 20;
+    const Max_Hours = 160;
+    
+    let total_emp_Hrs = 0;
+    let total_working_Days = 0;
+    let Emp_Wage_Array = new Array(); 
+    
+    function getWorkingHours(empCheck)
+    {
+    switch(empCheck) 
+        {
+        case Part_Time:
+            return Part_Time_Hrs;
+            
+        case Full_Time:
+            return Full_Time_Hrs;
+            
+        default:
+            return 0;
+            
+        }
+    }
+    
+    function calculateWages(empHrs)
+    {
+        return empHrs * Wage_Per_Hr
+    }
+
+    while(total_emp_Hrs <= Max_Hours && total_working_Days < Max_Working_Days)
+    {
+        total_working_Days++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        empHrs = getWorkingHours(empCheck);
+        total_emp_Hrs += getWorkingHours(empCheck); 
+        Emp_Wage_Array.push(calculateWages(empHrs));
+    }
+    
+    let empWage = calculateWages(total_emp_Hrs);
+    console.log("Total Hours: " + total_emp_Hrs + ", Emp Wage: " + empWage + ", Total Days: " + total_working_Days);
+    console.log(Emp_Wage_Array);
+}
+
 
