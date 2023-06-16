76f192c51577b581c8323b6072c3c89710eb7ecc
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-16 18:42:20 +0530

    [Vaibhav] Add:Calculate Daily Emp Wage

--- Employee Wage.js
+++ Employee Wage.js
@@ -1,10 +1,42 @@
-const IS_Absent = 1;
-var empCheck = Math.floor(Math.random() * 10) % 2;
-if(empCheck == IS_Absent)
+//UC1
+
 {
-    console.log("Enmployee is absent");
-    return;
+    const IS_Absent = 1;
+    var empCheck = Math.floor(Math.random() * 10) % 2;
+    if(empCheck == IS_Absent)
+    {
+        console.log("Enmployee is absent");
+        return;
+    }
+    else{
+        console.log("Employee is present");
+    }
 }
-else{
-    console.log("Employee is present");
+
+//UC2
+{
+    const Part_Time = 1;
+    const Full_Time = 2;
+    const Part_Time_Hrs = 4;
+    const Full_Time_Hrs = 8;
+    const Wage_Per_Hr = 20;
+
+    let empHrs = 0;
+    let Daily_Wage;
+
+    var empCheck = Math.floor(Math.random() * 10) % 3;
+    switch(empCheck) 
+    {
+        case Part_Time:
+            empHrs = Part_Time_Hrs;
+            break;
+        case Full_Time:
+            empHrs = Full_Time_Hrs;
+            break;
+        default:
+            empHrs = 0;
+            break;
+    }
+    Daily_Wage = empHrs * Wage_Per_Hr;
+    console.log("Employee Daily Wage is : " + Daily_Wage);
 }
