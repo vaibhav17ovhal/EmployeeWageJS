6049a54a29e93d8c4f9ce1ef1a9dfd4ca3c249d7
Vaibhav <vaibhavovhal15@gmail.com>
2023-06-16 18:48:35 +0530

    [Vaibhav] Add:Calculate Daily Emp Wage using Function

--- Employee Wage.js
+++ Employee Wage.js
@@ -40,3 +40,35 @@
     Daily_Wage = empHrs * Wage_Per_Hr;
     console.log("Employee Daily Wage is : " + Daily_Wage);
 }
+
+//UC3
+{
+    const Part_Time = 1;
+    const Full_Time = 2;
+    const Part_Time_Hrs = 4;
+    const Full_Time_Hrs = 8;
+    const Wage_Per_Hr = 20;
+
+    let empHrs = 0;
+    let Daily_Wage;
+
+    var empCheck = Math.floor(Math.random() * 10) % 3;
+    function getWorkingHours(empCheck)
+    {
+    switch(empCheck) 
+    {
+        case Part_Time:
+            return Part_Time_Hrs;
+            break;
+        case Full_Time:
+            return Full_Time_Hrs;
+            break;
+        default:
+            return 0;
+            break;
+    }
+    }
+    empHrs = getWorkingHours(empCheck);
+    Daily_Wage = empHrs * Wage_Per_Hr;
+    console.log("Employee Daily Wage is : " + Daily_Wage);  
+}
